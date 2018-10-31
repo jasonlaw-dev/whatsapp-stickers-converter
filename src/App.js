0036ad709b30a3547c9c3da7212e18b92e09c65f
Jason Law <jasonlaw.dev@gmail.com>
2018-10-31 20:11:06 +0800

    Add css, validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import ImageUpload from './ImageUpload';
+import Container from 'react-bootstrap/lib/Container';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import Navbar from 'react-bootstrap/lib/Navbar';
 
-import './App.css';
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { pictures: [] };
-        this.onDrop = this.onDrop.bind(this);
-    }
-
-    onDrop(picture) {
-        this.setState({
-            pictures: this.state.pictures.concat(picture),
-        });
     }
 
     render() {
         return (
-            <ImageUpload></ImageUpload>
+
+          <Container style={{marginTop: '20px'}}>
+            <Row className="justify-content-md-center">
+              <Col lg={10}>
+                <Navbar bg="light" expand="lg" style={{marginBottom: '20px'}}>
+                  <Navbar.Brand>WhatsApp Stickers Converter (Chrome Desktop only)</Navbar.Brand>
+                </Navbar>
+              </Col>
+            </Row>
+
+            <Row className="justify-content-md-center">
+              <Col lg={10}>
+                <ImageUpload></ImageUpload>
+              </Col>
+            </Row>
+          </Container>
+
         );
     }
 }
--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Form from 'react-bootstrap/lib/Form';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import Button from 'react-bootstrap/lib/Button';
+import ProgressBar from 'react-bootstrap/lib/ProgressBar';
 
 const jimp = require('jimp');
-const download = require('downloadjs')
+const download = require('downloadjs');
 
-function convertURIToWebpURI(URI) {
+function convertURIToWebpURI(URI, quality) {
   return new Promise(function(resolve, reject) {
     if (URI == null) return reject();
     var canvas = document.createElement('canvas'),
@@ -14,7 +19,41 @@ function convertURIToWebpURI(URI) {
       canvas.height = image.height;
       context.drawImage(image, 0, 0, canvas.width, canvas.height);
 
-      resolve(canvas.toDataURL("image/webp", 1));
+      resolve(canvas.toDataURL("image/webp", quality));
+    }, false);
+    image.src = URI;
+  });
+}
+
+function addTextToUri(URI, text) {
+  return new Promise(function(resolve, reject) {
+    if (URI == null) return reject();
+    var canvas = document.createElement('canvas'),
+      context = canvas.getContext('2d'),
+      image = new Image();
+    image.addEventListener('load', function() {
+      canvas.width = image.width;
+      canvas.height = image.height;
+
+      context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+      context.save();
+
+      context.beginPath();
+      context.arc(canvas.width - 0 - 16, canvas.height - 0 - 16, 16, 0, 2 * Math.PI);
+      // context.fillStyle = 'rgba(63,127,191,0.6)';
+      context.fillStyle = 'rgba(53, 67, 90, 0.85)';
+      context.fill();
+
+      context.restore();
+
+      context.font = '30px helvetica';
+      context.textAlign = 'center';
+      // context.fillStyle = '#262626';
+      context.fillStyle = '#f7f7f7';
+      context.lineWidth = 2;
+      context.fillText(text, canvas.width - 0 - 16, canvas.height - 0 - 6);
+      resolve(canvas.toDataURL("image/png"));
     }, false);
     image.src = URI;
   });
@@ -24,7 +63,18 @@ function convertURIToWebpURI(URI) {
 async function resizeAndConvert(input, px, toWebp) {
   return jimp.read(input).then(image => {
     if (toWebp) {
-      return image.contain(px, px).getBase64Async(jimp.MIME_PNG).then(convertURIToWebpURI);
+      return image.contain(px, px).getBase64Async(jimp.MIME_PNG)
+        .then(uri => convertURIToWebpURI(uri, 1).then(async (_webpUri) => {
+          let webpUri = _webpUri;
+
+          let quality = 1.0;
+          while (btoa(webpUri).length > 99999 && quality > 0.2) {
+            quality -= 0.08;
+            console.error(`WebP size ${Math.ceil(btoa(webpUri).length / 1024)}kb exceeded 100kb, resizing with quality ${quality}`);
+            webpUri = await convertURIToWebpURI(uri, quality);
+          }
+          return webpUri;
+        }))
     }
     return image.contain(px, px).getBase64Async(jimp.MIME_PNG);
   })
@@ -44,7 +94,7 @@ async function loadFile(file, type) {
       } catch (e) {
         reject(e);
       }
-    }
+    };
     reader.readAsArrayBuffer(file);
   }
 )
@@ -53,116 +103,190 @@ async function loadFile(file, type) {
 class ImageUpload extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      identifier: '',
+      name: '',
+      publisher: '',
+      trayFile: null,
+      stickersFiles: null,
+      isSubmitting: false,
+      progress: 0,
+      errorMsg: '',
+    };
   }
 
   _handleSubmit = (e) => {
     e.preventDefault();
 
+    this.setState({
+      progress: 0,
+      errorMsg: '',
+      isSubmitting: true,
+    });
+
     (async () => {
+
+
       const tray = await loadFile(this.state.trayFile, 'tray');
-      const stickers = [];
+      const stickersInPack = [];
+
+      const numOfPacks = Math.ceil(this.state.stickersFiles.length / 30);
+      const trays = [];
+
+      for (let pack = 0; pack < numOfPacks; pack++) {
+        stickersInPack.push([]);
+        trays.push(numOfPacks === 1 ? tray : await addTextToUri(tray, (pack + 1) + ''));
+      }
+
       for (let i = 0; i < this.state.stickersFiles.length; i++) {
-        stickers.push(await loadFile(this.state.stickersFiles[i], 'stickers'));
+        const sticker = await loadFile(this.state.stickersFiles[i], 'stickers');
+        this.setState({ progress: (i + 1) / this.state.stickersFiles.length * 100 });
+        stickersInPack[Math.floor(i / 30)].push(sticker);
       }
 
-      const dataJson = {
-        "identifier": this.state.identifier,
-        "name": this.state.name,
-        "publisher": this.state.publisher,
-        "tray_image": tray.replace('data:image/png;base64,', ''),
-        "stickers": [
-        ]
+      if (numOfPacks > 1 && stickersInPack[numOfPacks - 1].length < 3) {
+        stickersInPack[numOfPacks - 1] = [
+          ...stickersInPack[numOfPacks - 2].splice(-(3 - stickersInPack[numOfPacks - 1].length)),
+          ...stickersInPack[numOfPacks - 1],
+        ];
       }
 
-      dataJson.stickers.push(...stickers.map(sticker => ({
-      image_data: sticker.replace('data:image/webp;base64,', '')}
-      )));
+      stickersInPack.map((pack, index) => {
+        return {
+          'identifier': this.state.identifier + (index === 0 ? '' : '_' + (index + 1)),
+          'name': this.state.name + (index === 0 ? '' : ' (' + (index + 1) + ')'),
+          'publisher': this.state.publisher,
+          'tray_image': trays[index].replace('data:image/png;base64,', ''),
+          'stickers': pack.map(sticker => {
+            return {
+              image_data: sticker.replace('data:image/webp;base64,', ''),
+            }
+          }),
+        };
+      }).forEach(pack => {
+        download('data:application/json;base64,' + new Buffer(JSON.stringify(pack)).toString('base64'), `${pack.identifier}.json`, 'application/json')
+        this.setState({ isSubmitting: false });
+      });
 
-      download( 'data:application/json;base64,'+ new Buffer(JSON.stringify(dataJson)).toString('base64'), `${this.state.identifier}.json`, 'application/json');
-
-      console.log(JSON.stringify(dataJson));
-    })()
+    })().catch((e) => {
+      console.error(e);
+      this.setState({
+        errorMsg: e.message || e.toString(),
+        isSubmitting: false,
+      });
+    });
   };
 
   _handleImageChange = (e, type) =>{
     e.preventDefault();
 
-
     if (type === 'tray') {
       this.setState({
-        trayValue: e.target.value,
         trayFile: e.target.files[0]
       });
+
+
+      // for debugging photo
+      setTimeout(async() => {
+        const tray = await loadFile(this.state.trayFile, 'tray');
+        const tray2 = await addTextToUri(tray, '8');
+        this.setState({
+          imagePreviewUrl: tray2,
+        })
+      }, 500)
+
     } else if (type === 'stickers') {
       this.setState({
-        stickersValue: e.target.value,
         stickersFiles: e.target.files
       });
     }
 
-  }
+  };
 
-  _handleInputChange = (e, field) => {
+  _handleInputChange = (e) => {
     this.setState({
-      [field]: e.target.value
-    })
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  isFormValid = () => {
+    return this.state.identifier && this.state.name && this.state.publisher && this.state.trayFile != null && this.state.stickersFiles && this.state.stickersFiles.length >= 3;
+  };
+
+  isSubmittable = () => {
+    return this.isFormValid() && !this.state.isSubmitting;
   }
 
   render() {
-    let {imagePreviewUrl} = this.state;
-    let $imagePreview = null;
-    if (imagePreviewUrl) {
-      $imagePreview = (<img src={imagePreviewUrl} />);
-    } else {
-      $imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
-    }
-
     return (
-      <div className="previewComponent">
-        <form onSubmit={(e)=>this._handleSubmit(e)}>
+        <Form onSubmit={(e)=>this._handleSubmit(e)}>
+          <Form.Group as={Row} controlId="identifier">
+            <Form.Label column sm={4}>Identifier</Form.Label>
+            <Col sm={6}>
+              <Form.Control type="text" required onChange={this._handleInputChange} isInvalid={!/^[A-Za-z0-9-_.\s]*$/.test(this.state.identifier)} />
+              <Form.Control.Feedback type="invalid">
+                {'Alphanumeric characters with ".", "_", "-" or " " only '}
+              </Form.Control.Feedback>
+            </Col>
+          </Form.Group>
 
-          <div>
-            <label>
-              Identifier:
-              <input type="text" onChange={(e)=>this._handleInputChange(e, 'identifier')} />
-            </label>
-          </div>
+          <Form.Group as={Row} controlId="name">
+            <Form.Label column sm={4}>Pack Name</Form.Label>
+            <Col sm={6}>
+              <Form.Control type="text" required onChange={this._handleInputChange} />
+            </Col>
+          </Form.Group>
 
-          <div>
-            <label>
-              Pack Name:
-              <input type="text" onChange={(e)=>this._handleInputChange(e, 'name')} />
-            </label>
-          </div>
+          <Form.Group as={Row} controlId="publisher">
+            <Form.Label column sm={4}>Publisher</Form.Label>
+            <Col sm={6}>
+              <Form.Control type="text" required onChange={this._handleInputChange} />
+            </Col>
+          </Form.Group>
 
-          <div>
-            <label>
-              Publisher:
-              <input type="text" onChange={(e)=>this._handleInputChange(e, 'publisher')} />
-            </label>
-          </div>
+          <Form.Group as={Row} controlId="formTrayFile">
+            <Form.Label column sm={4}>Tray Icon</Form.Label>
+            <Col sm={8}>
+              <div className="custom-file">
+                <Form.Control
+                  className="custom-file-input"
+                  type="file"
+                  required
+                  isValid={false}
+                  onChange={(e)=>this._handleImageChange(e, 'tray')}
+                  accept="image/png"
+                />
+                <Form.Label className="custom-file-label">{this.state.trayFile ? this.state.trayFile.name : 'Choose file (PNG, any resolution)' }</Form.Label>
+              </div>
+            </Col>
+          </Form.Group>
 
-          <div>
-            <label>
-              Tray Icon PNG:
-              <input type="file" onChange={(e)=>this._handleImageChange(e, 'tray')} />
-            </label>
-          </div>
-          <div>
-            <label>
-              Stickers PNGs (3 - 30 images):
-              <input type="file" multiple onChange={(e)=>this._handleImageChange(e, 'stickers')} />
-            </label>
-          </div>
-          <hr/>
-          <button className="submitButton"
-                  type="submit"
-                  onClick={(e)=>this._handleSubmit(e)}>Convert Images</button>
-        </form>
-      </div>
+          <Form.Group as={Row} controlId="formStickersFiles">
+            <Form.Label column sm={4}>Stickers (3 or more images)</Form.Label>
+            <Col sm={8}>
+              <div className="custom-file">
+                <Form.Control
+                  className="custom-file-input"
+                  type="file"
+                  required
+                  multiple
+                  onChange={(e)=>this._handleImageChange(e, 'stickers')}
+                  accept="image/png"
+                />
+                <Form.Label className="custom-file-label">{this.state.stickersFiles && this.state.stickersFiles.length > 0 ? `${this.state.stickersFiles.length} file(s) selected` : 'Choose file (PNG, any resolution)' }</Form.Label>
+              </div>
+            </Col>
+          </Form.Group>
+          <Button type="submit" disabled={!this.isSubmittable()}>Convert Images</Button>
+          <ProgressBar style={{transition: 'none'}} now={this.state.progress} hidden={!this.state.isSubmitting && !this.state.errorMsg} variant={this.state.errorMsg ? 'danger' : ''} style={{marginTop: '15px'}} />
+          <p
+            className={this.state.errorMsg ? "text-danger" : "text-success"}
+            hidden={!this.state.errorMsg && this.state.progress !== 100}>{this.state.errorMsg || 'Images converted!'}</p>
+          <img src={this.state.imagePreviewUrl || ''} />
+        </Form>
     )
   }
 }
+
 
 export default ImageUpload
