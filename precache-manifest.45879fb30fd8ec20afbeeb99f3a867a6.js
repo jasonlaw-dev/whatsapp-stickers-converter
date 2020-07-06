89376415028f952bece95cdff6db8619fa6c8e3c
Jason Law <jasonlaw.dev@gmail.com>
2020-07-06 20:05:38 +0800

    Updates

---
+++ precache-manifest.45879fb30fd8ec20afbeeb99f3a867a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ffae1d65c0e0f78d20d",
+    "url": "/whatsapp-stickers-converter/static/css/main.d2244350.chunk.css"
+  },
+  {
+    "revision": "7ffae1d65c0e0f78d20d",
+    "url": "/whatsapp-stickers-converter/static/js/main.7ffae1d6.chunk.js"
+  },
+  {
+    "revision": "ef6efbc4a64ad7dcbfa5",
+    "url": "/whatsapp-stickers-converter/static/css/1.6d6495e3.chunk.css"
+  },
+  {
+    "revision": "ef6efbc4a64ad7dcbfa5",
+    "url": "/whatsapp-stickers-converter/static/js/1.ef6efbc4.chunk.js"
+  },
+  {
+    "revision": "c8d37ac8b74f6394f654",
+    "url": "/whatsapp-stickers-converter/static/js/runtime~main.c8d37ac8.js"
+  },
+  {
+    "revision": "c3e9a10129cef4c1416e915391a74e4c",
+    "url": "/whatsapp-stickers-converter/index.html"
+  }
+];
